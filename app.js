1f69ee17d46cc2ad0faadebd419f3ee92cea6c6f
Tony Nguyen <tonynguyen@Tony-Nguyen.local>
2019-01-03 20:55:37 -0800

    second commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const testPara = document.getElementById('#testPara'); 
+const title = document.getElementById('.titles'); 
+
+
+console.log(testPara); 
+
+let something = '2'; 
+
+titles = `this ${something} is the new title`; 
+
+
+
